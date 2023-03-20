991114f9eb0745612b4513dd2db3b7bb0001e5e3
Prasoon Soni <prasoonsoni03@gmail.com>
2023-03-20 17:09:05 +0530

    Added route to get all recipients

--- routes/hospitalRoutes.js
+++ routes/hospitalRoutes.js
@@ -3,5 +3,6 @@ import hospitalController from "../controllers/hospitalController.js"
 const router = express.Router()
 
 router.get('/donor', hospitalController.getAllDonors)
+router.get('/recipient', hospitalController.getAllRecipients)
 
 export default router
