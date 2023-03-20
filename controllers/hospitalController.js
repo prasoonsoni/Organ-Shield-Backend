93716429f88a61b15419e1f4b36c80c574af1945
Prasoon Soni <prasoonsoni03@gmail.com>
2023-03-20 18:58:04 +0530

    Added route to get all organs by recipient

--- controllers/hospitalController.js
+++ controllers/hospitalController.js
@@ -32,4 +32,15 @@ const getAllOrgansByDonor = async (req, res) => {
         return res.json({ status: false, message: "Internal Server Error Occurred" })
     }
 }
-export default { getAllDonors, getAllRecipients, getAllOrgansByDonor }
+
+const getAllOrgansByRecipient = async (req, res) => {
+    try {
+        const user_id = new ObjectId(req.params.id)
+        const organs = await Organ.find({ user_id: user_id })
+        return res.json({ success: true, message: "Organs Found Successfully", data: organs })
+    } catch (error) {
+        console.log(error.message)
+        return res.json({ status: false, message: "Internal Server Error Occurred" })
+    }
+}
+export default { getAllDonors, getAllRecipients, getAllOrgansByDonor, getAllOrgansByRecipient }
--- routes/hospitalRoutes.js
+++ routes/hospitalRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 router.get('/donor', hospitalController.getAllDonors)
 router.get('/donor/:id', hospitalController.getAllOrgansByDonor)
 router.get('/recipient', hospitalController.getAllRecipients)
+router.get('/recipient/:id', hospitalController.getAllOrgansByRecipient)
 
 export default router
